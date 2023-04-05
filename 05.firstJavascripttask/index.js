f9aae0ebd04867070a49dbbb28391619a1ccf8ee
Gunay <126713746+hummetova-gunay@users.noreply.github.com>
2023-04-05 03:06:46 -0700

    Add files via upload

---
+++ 05.firstJavascripttask/index.js
@@ -0,0 +1,24 @@
+console.log(4 + 2 + "5" + 2 + "a");
+console.log(12 * 8);
+let x = 10 / 2;
+console.log(x);
+let num1 = 20,
+  num2 = 17;
+console.log(num1 + num2);
+
+let name = "Gunay",
+  surname = "Hummatova",
+  year = "2001";
+console.log(name + "" + surname + "" + year);
+console.log(17 % 12);
+
+let cityName = "Baku";
+cityName = "Ganja";
+console.log(cityName);
+
+let copyObj = { name: "Gunay", surname: "Hummatova", age: 22 };
+console.log(copyObj);
+
+let obj1 = { cityName: "Ganja", uni: "GSU", qualification: "MAths and Info" };
+let obj2 = obj1;
+console.log(obj2);
