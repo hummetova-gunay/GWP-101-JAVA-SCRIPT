be856af4fa995ced169c03d386fa779a9e79fcb0
Gunay <126713746+hummetova-gunay@users.noreply.github.com>
2023-04-11 02:46:23 -0700

    Add files via upload

--- 10.objects.JS/script.js
+++ 10.objects.JS/script.js
@@ -1,15 +1,49 @@
-// first
-const obj = {
-    id: 1, 
-    fisrtName: "Gunay",
-    lastName: "Hummatova",
-    location: "GDU 2"
+//1
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//2
+const arr2 = [1, true, "Gunay"];
+//3
+const arr3 = [0, 1, 2, 3, 4, 5];
+const arr4 = arr2;
+//4
+const arr5 = [0, 1, 2, 3, 4, 5, 6, 7];
+arr5.push(8, 9);
+//5
+const arr6 = [];
+arr6.push(5, 6, 7);
+arr6[0] = 1;
+//6
+const arr7 = ["Gunay", "hummatova", 22, "Quba"];
+arr7[2] = "Qusar";
+//7
+for ( let num of numbers){
+    console.log(num)
 }
-//second
- const obj2= obj;
- //third
-  obj.id = 3;
-  console.log(obj)
-  //13
-  delete obj.location
-  console.log(obj)
+// 8
+const obj = {
+  id: 1,
+  fisrtName: "Gunay",
+  lastName: "Hummatova",
+  location: "GDU 2",
+};
+//9
+const obj2 = obj;
+//10
+obj.id = 3;
+console.log(obj);
+//13
+delete obj.location;
+console.log(obj);
+//
+//11
+for (let key in obj) {
+  console.log(obj[key]);
+}
+//12
+for (let key in obj) {
+  console.log(obj);
+}
+//14
+const newObj = {};
+newObj = Object.assign(obj);
+console.log(newObj);
